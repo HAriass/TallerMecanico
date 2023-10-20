5c93221f73d18b6f66b9edfb5e8a4eb92eadd011
HAriass <hernanarias.utn@gmail.com>
2023-10-20 11:03:20 -0300

    Agrego precio a servicio y orden

--- src/main/java/com/ProgramacionAvanzada/modelo/Servicio.java
+++ src/main/java/com/ProgramacionAvanzada/modelo/Servicio.java
@@ -5,9 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import java.io.Serializable;
 import lombok.Data;
@@ -30,5 +29,7 @@ public class Servicio implements Serializable{
     @NotBlank
     private String descripcion;
     
+    @Min(0)
+    private float precio;
     
 }
